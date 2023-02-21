f1d931e4e943b51de080ae3a9bb97ef2602756d0
nednguyen <rpmshockwave@gmail.com>
2023-02-21 11:39:40 -0500

    create project image and update user model

---
+++ app/models/project_image.py
@@ -0,0 +1,22 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from sqlalchemy.sql import func
+
+class ProjectImages(db.Model):
+  __tablename__ = 'projectImages'
+
+  if environment == "production":
+    __table_args__ = {'schema': SCHEMA}
+
+  id = db.Column(db.Integer, primary_key=True)
+  url = db.Column(db.String, nullable=False)
+  created_at = db.Column(db.DateTime, server_default=func.now(), nullable=False)
+  updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
+
+  project_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("projects.id")))
+
+
+def to_dict(self):
+  return {
+    "id": self.id,
+    "url": self.url
+  }
--- app/models/user.py
+++ app/models/user.py
@@ -10,8 +10,12 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(40), nullable=False)
+    last_name = db.Column(db.String(40), nullable=False)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    bio = db.Column(db.String(2000), nullable=False)
+    profile_image = db.Column(db.String, nullable=False)
     hashed_password = db.Column(db.String(255), nullable=False)
 
     @property
@@ -28,6 +32,10 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'bio': self.bio,
+            'profile_image': self.profile_image
         }
