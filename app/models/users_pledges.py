bde1528f2165eebee46d16e5f28853933205183f
nednguyen <rpmshockwave@gmail.com>
2023-02-23 13:35:40 -0500

    update users_pledges table

--- app/models/users_pledges.py
+++ app/models/users_pledges.py
@@ -1,8 +1,16 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
+
 users_pledges = db.Table(
   "users_pledges",
   db.Column("user_id", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), primary_key=True),
   db.Column("pledge_id", db.Integer, db.ForeignKey(add_prefix_for_prod("pledges.id")), primary_key=True)
 )
 
+if environment == "production":
+    __table_args__ = {'schema': SCHEMA}
+    users_pledges = db.Table(
+      "users_pledges",
+      db.Column("user_id", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), primary_key=True),
+      db.Column("pledge_id", db.Integer, db.ForeignKey(add_prefix_for_prod("pledges.id")), primary_key=True)
+    )
