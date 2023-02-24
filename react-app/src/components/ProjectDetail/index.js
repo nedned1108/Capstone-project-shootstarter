a283bd7652bc6516270d8cce98dbbeff8a173ec8
nednguyen <rpmshockwave@gmail.com>
2023-02-24 18:38:35 -0500

    finish css project detail page

--- react-app/src/components/ProjectDetail/index.js
+++ react-app/src/components/ProjectDetail/index.js
@@ -24,6 +24,10 @@ const ProjectDetail = () => {
     history.push('/')
     return 
   }
+  const func = (e) => {
+    const el = document.getElementsByClassName(e)
+    el[0].scrollIntoView()
+  }
 
   if (!currentProject) {
     return null
@@ -64,19 +68,41 @@ const ProjectDetail = () => {
           </div>
         </div>
       </div>
-      <div>
+      <div className="bottomMainDiv">
         <div>
-          <button>Story</button>
-          <button>Risks</button>
+          <div className="leftDiv">
+            <button className="storyButton" onClick={() => func("story")}>STORY</button>
+            <button className="riskButton" onClick={() => func("risks")}>RISKS</button>
+          </div>
         </div>
-        <div>
-          <div>{currentProject.story}</div>
+        <div className="middleDiv">
           <div>
+            <h2 className="story">Story</h2>
+            {currentProject.story}
+          </div>
+          <div className="middleDivImg">
             {currentProject.project_images.map(image => <img src={image.url}/>)}
+          </div>
+          <div>
+            <h2>Terms and Condition</h2>
+            By pledging to this project, you acknowledge that the final look, materials, and content of the rewards (and the project) are subject to change and may differ substantially from what is presented in this Kickstarter project.  
+            If you do not log in and confirm your pledge within 8 weeks of our notification via email to confirm your pledge, you agree that we have fulfilled our obligations to you in full for payment or donations received. 
+            However, we may, at our discretion, refund or ship your pledge if you contact us and confirm your pledge after the 8 week window has elapsed.  If you confirm your pledge within 8 weeks of our notification via email, 
+            you agree that our responsibility to you is to ship your order as entered into our pledge manager system, that title and risk passes to you upon delivery to a common carrier for such shipment, and you are responsible for import 
+            duties or any other duties that may be payable to the relevant tax authorities, providing correct address information and ensuring this address is deliverable by normal methods. We regret we are unable to ship to PO Boxes.  
+            If these terms are not acceptable to you, then we suggest that you do not participate in this crowdfunding campaign and acquire the game when it is released in stores. Thank you for your understanding.
+          </div>
+          <div>
+            <h2 className="risks">Risks</h2>
+            {currentProject.risks}
           </div>
         </div>
         <div>
-          <h3>Project owner info</h3>
+          <div className="rightDiv">
+            <img src={currentProject.owner.profile_image}/>
+            <h4>{currentProject.owner.first_name} {currentProject.owner.last_name}</h4>
+            <p>{currentProject.owner.bio}</p>
+          </div>
         </div>
       </div>
     </div>
