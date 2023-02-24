ea97278b334126202b79d8e9d7fc360f90151535
nednguyen <rpmshockwave@gmail.com>
2023-02-24 14:03:12 -0500

    update css project page

--- react-app/src/components/ProjectDetail/index.js
+++ react-app/src/components/ProjectDetail/index.js
@@ -5,6 +5,7 @@ import { thunkLoadAllProjects } from "../../store/project";
 import { thunkDeleteProject } from "../../store/project";
 import UpdateProjectModal from "../UpdateProjectModal";
 import OpenModalButton from "../OpenModalButton";
+import './ProjectDetail.css'
 
 
 const ProjectDetail = () => {
@@ -26,35 +27,35 @@ const ProjectDetail = () => {
   }
 
   return (
-    <div>
-      <div>
+    <div className="projectMainDiv">
+      <div className="projectTitle">
         <h2>{currentProject.project_name}</h2>
         <h3>{currentProject.description}</h3>
       </div>
-      <div>
-        <div>
+      <div className="projectImgInfo">
+        <div className="projectImg">
           <img src={currentProject.project_images[0].url}/>
         </div>
-        <div>
-          <div>
-            <h4>${currentProject.current_fund}</h4>
+        <div className="projectInfo">
+          <div className="projectInfoNum">
+            <h4 className="currentFund">${currentProject.current_fund}</h4>
             <p>pledged of ${currentProject.goal}</p>
             <h4>{currentProject.backers}</h4>
             <p>backers</p>
             <h4>{currentProject.end_day}</h4>
-            <div>
+            <div className="pledgeButton">
               <NavLink to={`/project/${currentProject.id}/pledge`}>
                 Back this project
               </NavLink>
             </div>
             {currentUser && currentUser.id == currentProject.owner_id ? 
-            <>
+            <div className="editDelete">
               <OpenModalButton 
                 buttonText="Update Project"
                 modalComponent={<UpdateProjectModal project={currentProject}/>}
               />
-              <button onClick={() => deleteProject(currentProject.id)}>Delete</button>
-            </>
+              <button onClick={() => deleteProject(currentProject.id)}>Delete Project</button>
+            </div>
             : '' 
             } 
           </div>
