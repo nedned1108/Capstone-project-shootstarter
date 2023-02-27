5ce17cb4b63b47bf5606f8156b58e2d1b7d5c980
nednguyen <rpmshockwave@gmail.com>
2023-02-27 14:18:18 -0500

    add comment route

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -4,3 +4,4 @@ from .project_form import ProjectForm
 from .pledge_form import PledgeForm
 from .project_image_form import ProjectImageForm
 from .choose_pledge_form import ChoosePledgeForm
+from .comment_form import CommentForm
