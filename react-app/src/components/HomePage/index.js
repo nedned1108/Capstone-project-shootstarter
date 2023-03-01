a5f254b9e692d6d62cdbc13fde624e5ac86cc804
nednguyen <rpmshockwave@gmail.com>
2023-03-01 18:50:40 -0500

    update

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -52,7 +52,7 @@ const HomePage = () => {
     dispatch(thunkLoadAllPledges())
   }, [dispatch])
 
-  if (projects.length == 0 && pledges.length == 0 && mostPledges !== undefined) {
+  if (projects.length == 0 && pledges.length == 0 && mostPledges == undefined) {
     return null
   }
   
