26ca6d2d391610bf9b876ff94e81dd7399e6d9a4
nednguyen <rpmshockwave@gmail.com>
2023-04-13 17:56:10 -0400

    gpt component

---
+++ react-app/src/components/ChatGPT/index.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+const ChatGPT = () => {
+
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>ChatGPT</h1>
+    </div>
+  )
+}
+
+export default ChatGPT;
